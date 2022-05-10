185501078b14de1992a51dd614fde51e7b57f4c8
Aman Dhruva Thamminana <48414198+amantham20@users.noreply.github.com>
2022-05-10 00:44:44 -0400

    firstish push

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <About />
+        <Experience />
+        <Contact />
+        <Footer />
+        
+
+        
+        
+    
+    </>
+  );
+}
+
+export default App
+
+
+
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import './header.css'
+
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
+
+const Header = () => {
+  return (
+   <header>
+     <div className='container header__container'>
+      <h5>Hello I'am</h5>
+      <h1>Aman Dhruva Thamminana</h1>
+      <h5 className='text-light'>I'm a Developer</h5> 
+
+      <CTA />
+      <HeaderSocials />
+
+      {/* <div className="me">
+        <img src={ME} alt='me'></img>
+      </div> */}
+
+      {/* <a href="#contact" className='scroll_down'> scroll down</a> */}
+
+
+
+
+     </div>
+   </header>
+  );
+}
+
+export default Header
